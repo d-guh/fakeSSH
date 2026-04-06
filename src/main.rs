e6f27355ec865d2b217a29c7ef66e1037e04d1b8
absprouse23 <trainguy2004@gmail.com>
2026-04-06 12:47:11 -0400

    Host key is generated on first run

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,32 @@ pub mod shell;
 use std::sync::Arc;
 
 use russh::keys::ssh_key::rand_core::OsRng;
+use russh::keys::ssh_key::LineEnding;
 use russh::keys::{Algorithm, PrivateKey};
 use russh::server::Server as _;
 use tokio::net::TcpListener;
 
 use auth::load_credentials;
 use server::Server;
+
+fn get_or_create_host_key(path: &str) -> Result<PrivateKey, Box<dyn std::error::Error>> {
+    let p = std::path::Path::new(path);
+    if p.exists() {
+        let pem = std::fs::read_to_string(p)?;
+        Ok(PrivateKey::from_openssh(pem.as_bytes())?)
+    } else {
+        let key = PrivateKey::random(&mut OsRng, Algorithm::Ed25519)?;
+        let pem = key.to_openssh(LineEnding::LF)?;
+        std::fs::write(p, pem.as_bytes())?;
+        #[cfg(unix)]
+        {
+            use std::os::unix::fs::PermissionsExt;
+            std::fs::set_permissions(p, std::fs::Permissions::from_mode(0o600))?;
+        }
+        log::info!("Generated new host key, saved to {path}");
+        Ok(key)
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -24,7 +44,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         inactivity_timeout: Some(std::time::Duration::from_secs(3600)),
         auth_rejection_time: std::time::Duration::from_secs(3),
         auth_rejection_time_initial: Some(std::time::Duration::from_secs(0)),
-        keys: vec![PrivateKey::random(&mut OsRng, Algorithm::Ed25519)?],
+        keys: vec![get_or_create_host_key("host_key.pem")?],
         ..Default::default()
     };
     let config = Arc::new(config);
